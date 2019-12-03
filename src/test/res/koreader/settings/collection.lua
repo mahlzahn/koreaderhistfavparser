1c8a976f5fea94e25d082503ca77c5dd1eede3a2
mahlzahn <6549824+mahlzahn@users.noreply.github.com>
2019-12-03 21:21:57 +0100

    add koreader settings files for testing

---
+++ src/test/res/koreader/settings/collection.lua
@@ -0,0 +1,13 @@
+-- we can read Lua syntax here!
+return {
+    ["favorites"] = {
+        [1] = {
+            ["file"] = "src/test/res/books/book1.epub",
+            ["order"] = 1
+        },
+        [2] = {
+            ["file"] = "src/test/res/books/book3.epub",
+            ["order"] = 2
+        }
+    }
+}
